89636ba627ee45ea96a0f669f6bfc9473959e065
Kolin-Ko <r07521706@g.ntu.edu.tw>
2019-08-08 12:16:13 +0800

    Updates

---
+++ precache-manifest.8061315c5969446d19a9cea60b5c603f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9195e63abf51c25c5a615f6825ed1a0d",
+    "url": "/monster/index.html"
+  },
+  {
+    "revision": "f4bbacccfb16c3ae6249",
+    "url": "/monster/static/css/main.98a8ef7b.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monster/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "f4bbacccfb16c3ae6249",
+    "url": "/monster/static/js/main.673a8720.chunk.js"
+  },
+  {
+    "revision": "24520cef794f93741fed",
+    "url": "/monster/static/js/runtime~main.488aa6c8.js"
+  }
+]);
